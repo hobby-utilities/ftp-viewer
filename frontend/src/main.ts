8bae7eaf6ebdea5350101ef942feb33174dae75a
elhongsh <elhongsh@gmail.com>
2023-10-29 00:49:59 +0900

    ➕ Add pinia package

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,17 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from "../router"
+import {createPinia} from "pinia"
 
 import PrimeVue from 'primevue/config'
 import 'primevue/resources/themes/lara-light-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(router);
+app.use(router)
+app.use(pinia)
 app.use(PrimeVue)
 
 app.mount('#app')
