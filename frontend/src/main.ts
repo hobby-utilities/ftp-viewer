75ef9a27dcaaaa98cf0058229fa05666bb85ad81
elhongsh <elhongsh@gmail.com>
2023-10-28 22:47:31 +0900

    ➕ Add primevue and its icon package

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,14 @@
 import {createApp} from 'vue'
 import App from './App.vue'
-import router from "../router";
+import router from "../router"
+
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/lara-light-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
 
 const app = createApp(App)
 
 app.use(router);
+app.use(PrimeVue)
 
 app.mount('#app')
